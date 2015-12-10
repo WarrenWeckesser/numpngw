b3a8daa6dbc5bd9d939cd7442c69549be31f7174
Warren Weckesser <warren.weckesser@gmail.com>
2015-12-09 19:56:29 -0500

    MAINT: Include python 3.5 in setup classifiers.

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
     py_modules=["numpngw"],
     install_requires=[
